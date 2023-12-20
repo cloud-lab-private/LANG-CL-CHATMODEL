4befdfdf6d60800ac713a73a1dfab1d1977b277e
JesterCharles <JesterCharles@gmail.com>
2023-12-20 15:13:47 -0500

    Adjustments based on feedback, clarified prompts, updated 'system_prompt.txt' and removed outdated instructions

--- src/main/lab.py
+++ src/main/lab.py
@@ -25,7 +25,7 @@ def send_single_human_message(message) -> BaseMessage:
     AIMessage object containing the LLM's response.
 
     Instructions:
-    - Create a new AzureChatOpenAI object with the deployment and model variable provided above.
+    - Create a new AzureChatOpenAI object with the model variable provided above.
     - Create a new HumanMessage object with the message variable provided above.
     - Send the HumanMessage object to the AzureChatOpenAI object.
     - Return the response from the AzureChatOpenAI object invoke method.
@@ -50,7 +50,7 @@ def send_human_message_prompt_template(style, message) -> BaseMessage:
     AIMessage object containing the LLM's response.
     
     Instructions:
-    - Create a new AzureChatOpenAI object with the deployment and model variable provided above.
+    - Create a new AzureChatOpenAI object with the model variable provided above.
     - Create a new HumanMessagePromptTemplate object with the message variable provided above.
     - Send the formatted messages from HumanMessagePromptTemplate object to through the invoke method of the AzureChatOpenAI object.
     - Return the response from the AzureChatOpenAI object.
@@ -65,7 +65,7 @@ def send_human_message_prompt_template(style, message) -> BaseMessage:
 
 def send_multi_message_prompt_template(style, message) -> BaseMessage:
     # System message prompt template
-    file = open("../../templates/system_prompt.txt")
+    file = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../templates/system_prompt.txt')))
     prompt_file = file.read()
     file.close()
 
@@ -81,7 +81,7 @@ def send_multi_message_prompt_template(style, message) -> BaseMessage:
     AIMessage object containing the LLM's response.
     
     Instructions:
-    - Create a new AzureChatOpenAI object with the deployment and model variable provided above.
+    - Create a new AzureChatOpenAI object with model variable provided above.
     - Create a new HumanMessagePromptTemplate object with the message variable provided above.
     - Create a new SystemMessagePromptTemplate object with the prompt_file variable provided above.
     - Create a new ChatPromptTemplate object with the HumanMessagePromptTemplate and SystemMessagePromptTemplate objects.
@@ -117,7 +117,7 @@ def send_prompt_with_chat_memory(style, message) -> List[BaseMessage]:
     AIMessage object containing the LLM's response.
 
     Instructions:
-        - Create a new AzureChatOpenAI object with the deployment and model variable provided above.
+        - Create a new AzureChatOpenAI object with the model variable provided above.
         - Create a new ChatPromptTemplate object using the from_messages method with a List contain a SystemMessagePromptTemplate object,
             a MessagesPlaceholder object with the 'history' value, and a HumanMessagePromptTemplate object.
         - Create a new ConversationBufferMemory object.
@@ -125,7 +125,8 @@ def send_prompt_with_chat_memory(style, message) -> List[BaseMessage]:
             object for the 'prompt' value, and the ConversationBufferMemory object for the 'memory' value in the chain.
         - Using the ConversationChain object, send each message through the predict method. Inputting each message object:
             message, message2 and message3.
-        - Return the contents of the 'history' by using ConversationBufferMemory object's load_memory_variables method.
+        - By using ConversationBufferMemory object's load_memory_variables method, we return an dictionary that contains
+            a key for 'history'. Extract & Return the value for the 'history' key, which is our List[BaseMessage] object.
 
     End TODO
     """
