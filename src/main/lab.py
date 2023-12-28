f74d4b415c9d1c56112ebfb06db06056e51e0cd9
JesterCharles <JesterCharles@gmail.com>
2023-12-28 15:48:03 -0500

    solution branch for the activity

--- src/main/lab.py
+++ src/main/lab.py
@@ -25,7 +25,7 @@ llm = HuggingFaceEndpoint(
 
 def send_single_human_message(message) -> BaseMessage:
     """
-    TODO: Implement this method to create a new AzureChatOpenAI object and invoke the chat object with a List
+    TODO: Implement this method to create a new ChatHuggingFace object and invoke the chat object with a List
             containing a single HumanMessage.
 
     :param:
@@ -35,10 +35,10 @@ def send_single_human_message(message) -> BaseMessage:
     AIMessage object containing the LLM's response.
 
     Instructions:
-    - Create a new AzureChatOpenAI object with the model variable provided above.
-    - Create a new HumanMessage object with the message variable provided above.
-    - Send the HumanMessage object to the AzureChatOpenAI object.
-    - Return the response from the AzureChatOpenAI object invoke method.
+    - Create a new ChatHuggingFace object with the llm variable provided above.
+    - Create a new HumanMessage object with the message variable provided via the arguments to this function.
+    - Send the HumanMessage object to the ChatHuggingFace object.
+    - Return the response from the ChatHuggingFace object invoke method.
 
     End TODO
     """
@@ -53,7 +53,7 @@ def send_single_human_message(message) -> BaseMessage:
 
 def send_human_message_prompt_template(style, message) -> BaseMessage:
     """
-    TODO: Create a HumanPromptTemplate object from the prompt file, sending the HumanMesasge to the AzureChatOpenAI object
+    TODO: Create a HumanPromptTemplate object from the prompt file, sending the HumanMesasge to the ChatHuggingFace object
             with the HumanPromptTemplate object's formatted messages.
     :param: 
     style: Takes in user input about what style they AI to respond in. (e.g. cheerful, formal, pirate, etc.)
@@ -63,10 +63,10 @@ def send_human_message_prompt_template(style, message) -> BaseMessage:
     AIMessage object containing the LLM's response.
     
     Instructions:
-    - Create a new AzureChatOpenAI object with the model variable provided above.
+    - Create a new ChatHuggingFace object with the model variable provided above.
     - Create a new HumanMessagePromptTemplate object with the message variable provided above.
-    - Send the formatted messages from HumanMessagePromptTemplate object to through the invoke method of the AzureChatOpenAI object.
-    - Return the response from the AzureChatOpenAI object.
+    - Send the formatted messages from HumanMessagePromptTemplate object to through the invoke method of the ChatHuggingFace object.
+    - Return the response from the ChatHuggingFace object.
     
     End TODO
     """
@@ -97,11 +97,11 @@ def send_multi_message_prompt_template(style, message) -> BaseMessage:
     AIMessage object containing the LLM's response.
     
     Instructions:
-    - Create a new AzureChatOpenAI object with model variable provided above.
+    - Create a new ChatHuggingFace object with model variable provided above.
     - Create a new HumanMessagePromptTemplate object with the message variable provided above.
     - Create a new SystemMessagePromptTemplate object with the prompt_file variable provided above.
     - Create a new ChatPromptTemplate object with the HumanMessagePromptTemplate and SystemMessagePromptTemplate objects.
-    - Send the formatted messages from PromptTemplate object to the AzureChatOpenAI object via the invoke method, returning 
+    - Send the formatted messages from PromptTemplate object to the ChatHuggingFace object via the invoke method, returning 
         the Message from the AI.
     
     End TODO
@@ -111,7 +111,7 @@ def send_multi_message_prompt_template(style, message) -> BaseMessage:
     human_message = HumanMessagePromptTemplate.from_template(message)
     system_message = SystemMessagePromptTemplate.from_template(prompt_file)
     chat_prompt = ChatPromptTemplate.from_messages([human_message, system_message])
-    response = chat_model.invoke(chat_prompt.format())
+    response = chat_model.invoke(chat_prompt.format(style=style, message=message))
     return response
 
     # Replace with return statement
@@ -125,7 +125,7 @@ def send_prompt_with_chat_memory(style, message) -> List[BaseMessage]:
 
     """
     TODO: The send_prompt_with_chat_memory function is designed to facilitate a conversation with the Language 
-    Learning Model (LLM). It creates an AzureChatOpenAI object and a ChatPromptTemplate object, which includes a 
+    Learning Model (LLM). It creates an ChatHuggingFace object and a ChatPromptTemplate object, which includes a 
     SystemMessagePromptTemplate, a MessagesPlaceholder for history, and a HumanMessagePromptTemplate. The function 
     also initializes a ConversationBufferMemory object to store the conversation history. It then creates a 
     ConversationChain object and sends messages through the run method. Finally, it returns the conversation 
@@ -139,28 +139,26 @@ def send_prompt_with_chat_memory(style, message) -> List[BaseMessage]:
     AIMessage object containing the LLM's response.
 
     Instructions:
-        - Create a new AzureChatOpenAI object with the model variable provided above.
-        - Create a new ChatPromptTemplate object using the from_messages method with a List contain a SystemMessagePromptTemplate object,
-            a MessagesPlaceholder object with the 'history' value, and a HumanMessagePromptTemplate object.
+        - Create a new ChatHuggingFace object with the model variable provided above.
         - Create a new ConversationBufferMemory object.
-        - Create a new ConversationChain object with the AzureChatOpenAI object for the 'llm' value, the ChatPromptTemplate
-            object for the 'prompt' value, and the ConversationBufferMemory object for the 'memory' value in the chain.
-        - Using the ConversationChain object, send each message through the predict method. Inputting each message object:
-            message, message2 and message3.
-        - By using ConversationBufferMemory object's load_memory_variables method, we return an dictionary that contains
-            a key for 'history'. Extract & Return the value for the 'history' key, which is our List[BaseMessage] object.
+        - Create a new ConversationChain object with the ChatHuggingFace object for the 'llm' value and the 
+            ConversationBufferMemory object for the 'memory' value in the chain.
+        - Using the ConversationChain object, send each message through the predict method for a total of 3 predict executions.
+        - Return the ConversationBufferMemory object's buffer_as_messages attribute. Which is a list of BaseMessage objects.
 
     End TODO
     """
     # Write Code Below
     chat_model = ChatHuggingFace(llm=llm)
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [SystemMessagePromptTemplate.from_template(system_ai_template), MessagesPlaceholder("history"),
-         HumanMessagePromptTemplate.from_template(message)])
-    conversation_buffer = ConversationBufferMemory()
-    conversation_chain = ConversationChain(llm=chat_model, prompt=chat_prompt, memory=conversation_buffer)
-    conversation_chain.predict([message, message2, message3])
-    return conversation_buffer.load_memory_variables()['history']
+    memory = ConversationBufferMemory()
+    conversation = ConversationChain(
+        llm=chat_model,
+        memory=memory,
+    )
+    conversation.predict(input=message)
+    conversation.predict(input=message2)
+    conversation.predict(input=message3)
+    return memory.buffer_as_messages
 
     # Replace with return statement
     # raise NotImplementedError("This function has not been implemented yet.")
--- src/test/test_lab.py
+++ src/test/test_lab.py
@@ -1,36 +1,32 @@
-import os
 import unittest
 from typing import List
 
-from langchain.chat_models import AzureChatOpenAI
-from langchain_community.chat_models import ChatHuggingFace
-from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 
 from src.main.lab import send_single_human_message, send_human_message_prompt_template, send_multi_message_prompt_template, \
     send_prompt_with_chat_memory
-from src.utilities.llm_testing_util import llm_connection_check, llm_wakeup
+from src.utilities.llm_testing_util import llm_connection_check, llm_wakeup, classify_relevancy
 
 
 class TestLLMResponses(unittest.TestCase):
-    def test_llm_connection(self):
+
+    def test_llm_sanity_check(self):
+        """
+        This function is a sanity check for the Language Learning Model (LLM) connection.
+        It attempts to generate a response from the LLM. If a 'Bad Gateway' error is encountered,
+        it initiates the LLM wake-up process. This function is critical for ensuring the LLM is
+        operational before running tests and should not be modified without understanding the
+        implications.
+        Raises:
+            Exception: If any error other than 'Bad Gateway' is encountered, it is raised to the caller.
+        """
         try:
             response = llm_connection_check()
             print(response)
         except Exception as e:
             if 'Bad Gateway' in str(e):
                 llm_wakeup()
-
-    def test_chat_model(self):
-        llm = HuggingFaceEndpoint(
-            endpoint_url="https://z8dvl7fzhxxcybd8.eu-west-1.aws.endpoints.huggingface.cloud",
-            task="text2text-generation",
-            model_kwargs={
-                "max_new_tokens": 200
-            }
-        )
-        chat_model = ChatHuggingFace(llm=llm)
-        print(chat_model.invoke([HumanMessage(content="Hello, how are you?")]))
+                self.fail("LLM is not awake. Please try again in 3-5 minutes.")
 
     def test_send_single_human_message(self):
         response = send_single_human_message("Hello, how are you?")
@@ -45,9 +41,9 @@ class TestLLMResponses(unittest.TestCase):
         self.assertIsInstance(response, AIMessage)
 
     def test_send_prompt_with_chat_memory(self):
-        response = send_prompt_with_chat_memory("pirate", "How much wood could a woodchuck chuck if a woodchuck could chuck wood?")
+        response = send_prompt_with_chat_memory("pirate", "What is a woodchuck?")
         self.assertIsInstance(response, List)
-        self.assertIn("woodchuck", response[1].content.lower())
+        self.assertTrue(classify_relevancy(response[1], "Tell me about wo"))
 
 
 if __name__ == '__main__':
